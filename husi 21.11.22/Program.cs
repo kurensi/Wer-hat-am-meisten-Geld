c2267a201da3e77167ae7e8b9a8c7f9a2d76c993
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:21:29 +0100

    Projektdateien hinzufügen.

---
+++ husi 21.11.22/Program.cs
@@ -0,0 +1,14 @@
+﻿//Eingabe
+Console.Write($"Geben sie eine Zahl zwischen 10 und 20 ein: ");
+int x=Convert.ToInt32(Console.ReadLine());
+while (true) {
+    if(x >= 10 && x < 20)
+   Console.Write($"{x} ist korrekt");
+    Console.ReadKey();
+    continue;
+    else
+    {
+        Console.WriteLine($"Die eingabe war falsch");
+    }
+  
+}
---
+++ if HA/Geld frage.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Person 1 Geld: ");
+Int32 x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Person 2 Geld: ");
+Int32 y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Person 3 Geld");
+Int32 z = Convert.ToInt32(Console.ReadLine());
+try
+{
+    if (x >= y && y >= z)
+        Console.WriteLine($"Die Person 1 hat am meisten Geld: {x}");
+    else if (y >= z && z >= y)
+        Console.WriteLine($"Die Person 2 hat am meisten geld : {y}");
+    else if (z >= x && x >= y)
+        Console.WriteLine($"Die Person 3 hat am meisten geld : {z}");
+
+}
+
+catch (Exception e)
+{ 
+    Console.WriteLine(e.Message); 
+}
+Console.ReadKey();
